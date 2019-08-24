f3afd8e234e678ae443ca4622631709d58a4cebf
jaredrwight <20687190+Trail3lazer@users.noreply.github.com>
2019-08-24 12:07:22 -0600

    FIXED user.psswrd datatype err

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
     return sequelize.define("users", {
         type: DataTypes.STRING, // NOT NULL Landlord, tenant
         username: DataTypes.TEXT, //NOT NULL
-        password: DataTypes.Text, // Not Null
+        password: DataTypes.TEXT, // Not Null
         authToken: DataTypes.STRING //Passport generated
       })
     
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,32 +4,45 @@ module.exports = function (app) {
   let arr = ['places', 'users', 'comments']
 
   for (i in arr) {
+
+
     // Create
     app.post(`/api/${arr[i]}`, function (req, res) {
-      db[arr[i]].create(req.body).then(function (table) {
+      db[arr[i]].create(req.body)
+      .then(function (table) {
         res.json(table);
       });
     });
 
     // Read
     app.get(`/api/${arr[i]}`, function (req, res) {
-      db[arr[i]].findAll({}).then(function (table) {
+      db[arr[i]].findAll({})
+      .then(function (table) {
         res.json(table);
       });
     });
 
     // Update
     app.put(`/api/${arr[i]}/:id`, function (req, res) {
-      db[arr[i]].update({ where: { id: req.params.id } }).then(function (table) {
-        res.json(table);
+      db[arr[i]].update({ 
+        where: { 
+          id: req.params.id } })
+          .then(function (table) {
+            res.json(table);
       });
     });
 
     // Delete
     app.delete(`/api/${arr[i]}/:id`, function (req, res) {
-      db[arr[i]].destroy({ where: { id: req.params.id } }).then(function (table) {
-        res.json(table);
+      db[arr[i]].destroy({ 
+        where: { 
+          id: req.params.id } })
+          .then(function (table) {
+            res.json(table);
       });
     });
+
+
   }
+
 };
