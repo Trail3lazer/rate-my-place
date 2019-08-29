382b412a607412504c676f17118c9741feb1ab1b
davidbridges123 <david.bridges@live.com>
2019-08-28 21:08:24 -0600

    update

--- models/users.js
+++ models/users.js
@@ -1,17 +1,9 @@
-// module.exports = (sequelize, DataTypes) => {
-//     return sequelize.define("users", {
-//         type: DataTypes.STRING, // NOT NULL Landlord, tenant
-//         username: DataTypes.TEXT, //NOT NULL
-//         password: DataTypes.TEXT, // Not Null
-//         authToken: DataTypes.STRING //Passport generated
-//       })
-    
-// }
-
-module.exports = function(sequelize, Sequelize) {
- 
-  var User = sequelize.define('user', {
-
+module.exports = (sequelize, Sequelize, DataTypes) => {
+    return sequelize.define("user", {
+        // type: DataTypes.STRING, // NOT NULL Landlord, tenant
+        // username: DataTypes.TEXT, //NOT NULL
+        // password: DataTypes.TEXT, // Not Null
+        // authToken: DataTypes.STRING //Passport generated
     id: { autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER},
 		firstname: { type: Sequelize.STRING,notEmpty: true},
 		lastname: { type: Sequelize.STRING,notEmpty: true},
@@ -21,10 +13,29 @@ module.exports = function(sequelize, Sequelize) {
 		password : {type: Sequelize.STRING,allowNull: false }, 
 		last_login: {type: Sequelize.DATE},
     status: {type: Sequelize.ENUM('active','inactive'),defaultValue:'active' }
+      })
+    };
+
+    
+// }
+
+// module.exports = function(sequelize, Sequelize) {
+ 
+//   var User = sequelize.define('user', {
+
+//     id: { autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER},
+// 		firstname: { type: Sequelize.STRING,notEmpty: true},
+// 		lastname: { type: Sequelize.STRING,notEmpty: true},
+// 		username: {type:Sequelize.TEXT},
+// 		about : {type:Sequelize.TEXT},
+// 		email: { type:Sequelize.STRING, validate: {isEmail:true} },
+// 		password : {type: Sequelize.STRING,allowNull: false }, 
+// 		last_login: {type: Sequelize.DATE},
+//     status: {type: Sequelize.ENUM('active','inactive'),defaultValue:'active' }
 
 
-  });
+//   });
 
-  return User;
+//   return User;
 
-}
+// }
